33ec27be00422163374c67f9d6a3c78bcfa4c397
wanz <wangzhigang1999@live.cn>
2021-05-31 10:27:18 +0800

    增加第二阶段支持

--- my_test.py
+++ my_test.py
@@ -1,15 +1,18 @@
 import os
 
-import cv2
 import torch
+from PIL import Image
 from tqdm import tqdm
 
 from models.experimental import attempt_load
-from util import load_image, xywh2xyxy
+from util import load_image, xywh2xyxy, pre_crop
 
 dir = r"E:\222\ElectricityMeter\training\images"
 
-def test(weights, cuda=False):
+w_h_rate = 5.0
+
+
+def test(weights, cuda=True):
     # Initialize/load model and set device
     imgsz = 640
     device = torch.device('cuda:0' if cuda else 'cpu')
@@ -18,8 +21,7 @@ def test(weights, cuda=False):
     model.half()
     # Configure
     model.eval()
-    # model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    model(torch.zeros(1, 3, imgsz, imgsz).to(device))  # run once
+    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
 
     for i in tqdm(os.listdir(dir)):
 
@@ -30,7 +32,7 @@ def test(weights, cuda=False):
         img = torch.unsqueeze(img, 0)
 
         with torch.no_grad():
-            out, _ = model(img, augment=False)  # inference and training outputs
+            out, _ = model(img, augment=False)
             predn = out[0]
             max_conf, box = 0, []
             for *xyxy, conf, _ in predn.tolist():
@@ -40,23 +42,36 @@ def test(weights, cuda=False):
                     continue
             box = [int(i) for i in xywh2xyxy(box)]
 
-            left, top, right, bottom = box[0], box[1], box[2], box[3]
-            left -= 5
-            left = max(left, 0)
-            top -= 8
-            right += 5
-            bottom += 8
+            # left, top, right, bottom = box[0], box[1], box[2], box[3]
+            try:
+                img = Image.open(os.path.join(dir, i))
+                res, _, _ = pre_crop(img, box)
+                res = res.resize((500, 100))
+                res.save("{}/{}".format("crop", i))
+            except:
+                pass
+            # img = np.asarray(res)
+            # cv2.imshow("sss",img)
+            # cv2.waitKey(0)
+            # res.show()
+            # print(w, h)
+            # left -= 5
+            # left = max(left, 0)
+            # top -= 8
+            # right += 5
+            # right = min(right, 640)
+            # bottom += 8
 
-            img = cv2.imread(os.path.join(dir, i))
-            res = cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            cropImg = res[top:bottom, left:right]
-
-            cv2.imshow("src", res)
+            # img = cv2.imread(os.path.join(dir, i))
+            # res = cv2.rectangle(img, (left, top), (right, bottom), (0, 0, 255), 2)
+            #
+            # cropImg = res[top:bottom, left:right]
+            # print(i)
+            # cv2.imshow("src", res)
             # cv2.imshow("crop", cropImg)
-            # cv2.imwrite("{}/{}".format("bar_data", i), cropImg)
-            cv2.waitKey(0)
+            # cv2.imwrite("{}/{}".format("crop", i), cropImg)
+            # cv2.waitKey(0)
 
 
 if __name__ == '__main__':
-    test(r"best.pt")
+    test(r"C:\Users\wanz\PycharmProjects\machine_learning\best.pt")
--- util.py
+++ util.py
@@ -1,18 +1,14 @@
 import cv2
 import numpy as np
-import torch
+from torchvision.transforms import *
 
 
 def load_image(path):
     img = cv2.imread(path)  # BGR
-
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     img = np.stack((binary,) * 3, axis=-1)
-    # cv2.imshow("222", img)
-    # cv2.waitKey(0)
-
     img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
     img = np.ascontiguousarray(img)
     return torch.from_numpy(img)
@@ -21,8 +17,66 @@ def load_image(path):
 def xywh2xyxy(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
-    y[ 0] = x[0] - x[ 2] / 2  # top left x
-    y[ 1] = x[1] - x[ 3] / 2  # top left y
-    y[ 2] = x[0] + x[ 2] / 2  # bottom right x
-    y[ 3] = x[1] + x[ 3] / 2  # bottom right y
+    y[0] = x[0] - x[2] / 2  # top left x
+    y[1] = x[1] - x[3] / 2  # top left y
+    y[2] = x[0] + x[2] / 2  # bottom right x
+    y[3] = x[1] + x[3] / 2  # bottom right y
     return y
+
+
+def expand_box(box, rate=5):
+    y_min = int(box[1])
+    y_max = int(box[3])
+    x_min = int(box[0])
+    x_max = int(box[2])
+    ref_width = x_max - x_min
+    x_max = int(float(x_max) + 0.05 * ref_width)  # 0.2
+    x_min = int(float(x_min) - 0.1 * ref_width)  # 0.15
+
+    real_width = x_max - x_min
+
+    real_height = real_width / 5 / 2
+
+    y_center = int((y_min + y_max) / 2)
+
+    y_max = y_center + real_height
+    y_min = y_center - real_height
+
+    return [x_min, y_min, x_max, y_max]
+
+
+def get_box_padding(rect, h, w):
+    l, t, r, b = 0, 0, 0, 0
+    if rect[0] < 0:
+        l = -rect[0]
+    if rect[1] < 0:
+        t = -rect[1]
+    if rect[2] > w:
+        r = rect[2] - w
+    if rect[3] > h:
+        b = rect[3] - h
+    return [l, t, r, b]
+
+
+def pre_crop(im_pil, face_rect):
+    w, h = im_pil.size
+    sq_rect = expand_box(face_rect)
+    padding = get_box_padding(sq_rect, h, w)
+    transform_list = []
+    if set(padding) != {0}:
+        transform_list.append(transforms.Pad(tuple(padding), padding_mode='constant'))
+        x1, y1, x2, y2 = sq_rect
+        _w, _h = x2 - x1, y2 - y1
+        x1 = max(0, x1)
+        y1 = max(0, y1)
+        x2 = x1 + _w
+        y2 = y1 + _h
+        crop_rect = (x1, y1, x2, y2)
+    else:
+        crop_rect = tuple(sq_rect)
+    transform_list.append(transforms.Lambda(lambda img: img.crop(crop_rect)))
+    trans = transforms.Compose(transform_list)
+
+    im_crop = trans(im_pil.copy())
+
+    return im_crop, padding, sq_rect
